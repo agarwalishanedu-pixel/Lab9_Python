1110719dc9b5ffd77f4750bbb53bb9f7b7ef3177
Ishan <agarwal.ishanedu@gmail.com>
2026-03-15 20:47:09 -0400

    Create player file and import

---
+++ player.py
@@ -0,0 +1,10 @@
+"""
+Program Name: Lab 9 (player.py)
+My name: Ishan Agarwal
+Purpose: This program is meant to deepen my understanding of classes in python. Also this is used to make a game that flips coins.
+Starter Code: None
+Date: 03/15/2026 
+"""
+
+from coin import Coin
+
