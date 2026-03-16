f3e8429557f3e8d93b8e52d320754156dccec5f7
Ishan <agarwal.ishanedu@gmail.com>
2026-03-15 22:02:04 -0400

    Make file + make main function + import player

---
+++ Lab9_iagarwal-1.py
@@ -0,0 +1,15 @@
+"""
+Program Name: Lab 9 (Main File)
+My name: Ishan Agarwal
+Purpose: This program is meant to deepen my understanding of classes in python. Also this is used to make a game that flips coins.
+Starter Code: None
+Date: 03/15/2026 
+"""
+
+#import player class
+from player import Player
+
+#Make the main function
+def main():
+
+    print()
