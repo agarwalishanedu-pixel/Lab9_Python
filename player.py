2c83b84d76e041698f2f91645e465d473a7514f0
Ishan <agarwal.ishanedu@gmail.com>
2026-03-15 21:07:21 -0400

    Creates the first few functions

--- player.py
+++ player.py
@@ -8,3 +8,23 @@ Date: 03/15/2026
 
 from coin import Coin
 
+class Player:
+    # This class is representing a player
+
+    def __init__(self, name):
+        """
+        This initializes the player
+        """
+
+        self.__name = name
+        self.__wallet = 20
+        self.__coin = Coin()
+
+    def toss_coin(self):
+        """
+        This tosses the coin
+        """
+
+        self.__coin.toss()
+
+    
