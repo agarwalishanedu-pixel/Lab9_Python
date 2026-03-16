b5787cec3c873445b92dacea971453d5cba97661
Ishan <agarwal.ishanedu@gmail.com>
2026-03-15 22:20:29 -0400

    Finished the loop inside the function

--- Lab9_iagarwal-1.py
+++ Lab9_iagarwal-1.py
@@ -32,5 +32,31 @@ def main():
         player1.toss_coin()
         player2.toss_coin()
 
+        #it sets a variable to store value
+        sidep1 = player1.get_coin_side
+        sidep2 = player2.get_coin_side
+
+        print(f"{player1.get_name()} tossed {sidep1}")
+        print(f"{player2.get_name()} tossed {sidep2}")
+
+        #checks for who wins, if both same, then player 1, otherwise player 2 wins
+        if sidep1 == sidep2:
+            player1.win_coin()
+            player2.lose_coin()
+            print("...It's a Match! Player 1 wins a coin.")
+        else:
+            player1.lose_coin()
+            player2.win_coin()
+            print("...No Match! Player 2 wins a coin.")
+
+        print("")
+        print(f"{player1.get_name()} has {player1.get_wallet()} coins.")
+        print(f"{player1.get_name()} has {player1.get_wallet()} coins.")
+        print("")
+
+        play = input("Do you want to toss the coins? (y/n): ")
+        
+        if(player1.get_wallet() == 0 or player2.get_wallet() == 0):
+            play = "n"
 
 main()
