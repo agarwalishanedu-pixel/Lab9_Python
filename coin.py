fcc0fa88dcbd275bd99cfd5bd1265e0b7b86901c
Ishan <agarwal.ishanedu@gmail.com>
2026-03-15 20:38:33 -0400

    main

---
+++ coin.py
@@ -0,0 +1,14 @@
+"""
+Program Name: Lab 9 (coin.py)
+My name: Ishan Agarwal
+Purpose: This program is meant to deepen my understanding of classes in python. Also this is used to make a game that flips coins.
+Starter Code: None
+Date: 03/15/2026 
+"""
+
+import random
+
+#Create the class
+class Coin:
+    # This class is representing a single tossable coin
+    
